9d8d4f9e5be939ff12c0f6a0130fb01ab1505a1f
Richard Joo <richardjoo@gmail.com>
2011-08-22 15:58:38 -0700

    created myfirstapp in a snap! haha

---
+++ myfirstapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myfirstapp::Application.config.secret_token = '83463ee71f4a2d6712957f5c01b2cc81556657d8b56eeccd898e4a8a0720a31a4e1aefe02b28fa6fb0e3211450809ef7cc50e10397529cdb39cb02b6d5121e67'
